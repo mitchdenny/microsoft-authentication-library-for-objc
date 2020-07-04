d185dc53f59d8bdc863b381f3e666c297b15ae6e
Mitch Denny <mitch@mitchdenny.com>
2020-07-04 17:01:01 +1000

    Add URL with checksum.

--- Package.swift
+++ Package.swift
@@ -13,6 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MSAL",
-            path: "artifacts/MSAL.xcframework.zip")
+            url: "https://github.com/mitchdenny/microsoft-authentication-library-for-objc/releases/download/1.1.6-beta.1/MSAL.xcframework.zip",
+            checksum: "57cca1fa07c9bf0ce4e0c7e367a7530cdb08bb8399cd00ff7cf7b8f12f45849d"
+            )
     ]
 )
